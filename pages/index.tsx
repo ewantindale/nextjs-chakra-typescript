c8a1bdcfe5cdc96b09a2bb014579b62945c29c91
Ewan Tindale <ewannnnn@gmail.com>
2020-09-03 23:10:38 +0100

    add welcome message

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
+import { Flex, Text } from "@chakra-ui/core";
+
 const Home = () => {
-  return <div>hello from next.js</div>;
+  return (
+    <Flex h="100vh" justify="center" align="center">
+      <Text fontSize="6xl" bg="tomato" p="5">
+        hello from next.js, chakra, and typescript
+      </Text>
+    </Flex>
+  );
 };
 
 export default Home;
